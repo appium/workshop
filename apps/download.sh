82fee78fdad5b25da6fa71df5a96b4799999ea86
Jonathan Lipps <jlipps@gmail.com>
2013-10-01 12:36:10 +0100

    add gappium app

--- apps/download.sh
+++ apps/download.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 thisdir=$(dirname $0)
 pushd $thisdir
-curl -O http://appium.s3.amazonaws.com/SauceDashboard.app.zip
-curl -O http://appium.s3.amazonaws.com/SauceDashboard.apk
+#curl -O http://appium.s3.amazonaws.com/SauceDashboard.app.zip
+#curl -O http://appium.s3.amazonaws.com/SauceDashboard.apk
 curl -O http://appium.s3.amazonaws.com/TestApp6.1.app.zip
 curl -O http://appium.s3.amazonaws.com/ContactManager.apk
+curl -O http://appium.s3.amazonaws.com/HelloGappium.app.zip
 popd
